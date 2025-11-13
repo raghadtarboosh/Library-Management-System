b79347850a829db7adca415ca440d478a2c4e307
BaraahAbdoh <165507378+BaraahAbdoh@users.noreply.github.com>
2025-11-13 11:33:16 +0300

    the first step

---
+++ Library Management System/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Library_Management_System
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.flowLayoutPanelIcons = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.linkForgot = new System.Windows.Forms.LinkLabel();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.bictureIllustration = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panelLogin.SuspendLayout();
+            this.flowLayoutPanelIcons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bictureIllustration)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Controls.Add(this.flowLayoutPanelIcons);
+            this.panelLogin.Controls.Add(this.btnLogin);
+            this.panelLogin.Controls.Add(this.linkForgot);
+            this.panelLogin.Controls.Add(this.chkRemember);
+            this.panelLogin.Controls.Add(this.txtPassword);
+            this.panelLogin.Controls.Add(this.txtEmail);
+            this.panelLogin.Controls.Add(this.lblTitle);
+            this.panelLogin.Location = new System.Drawing.Point(60, 90);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(380, 420);
+            this.panelLogin.TabIndex = 0;
+            this.panelLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLogin_Paint);
+            // 
+            // flowLayoutPanelIcons
+            // 
+            this.flowLayoutPanelIcons.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanelIcons.Controls.Add(this.pictureBox2);
+            this.flowLayoutPanelIcons.Controls.Add(this.pictureBox3);
+            this.flowLayoutPanelIcons.Location = new System.Drawing.Point(17, 323);
+            this.flowLayoutPanelIcons.Name = "flowLayoutPanelIcons";
+            this.flowLayoutPanelIcons.Size = new System.Drawing.Size(342, 77);
+            this.flowLayoutPanelIcons.TabIndex = 6;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(72, 255);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(219, 48);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // linkForgot
+            // 
+            this.linkForgot.AutoSize = true;
+            this.linkForgot.Location = new System.Drawing.Point(229, 172);
+            this.linkForgot.Name = "linkForgot";
+            this.linkForgot.Size = new System.Drawing.Size(118, 17);
+            this.linkForgot.TabIndex = 4;
+            this.linkForgot.TabStop = true;
+            this.linkForgot.Text = "Forgot Password?";
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.Location = new System.Drawing.Point(23, 172);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(159, 21);
+            this.chkRemember.TabIndex = 3;
+            this.chkRemember.Text = "Remember Password";
+            this.chkRemember.UseVisualStyleBackColor = true;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(89, 113);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(189, 24);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(89, 69);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(189, 24);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(158, 25);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(41, 17);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Login";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // bictureIllustration
+            // 
+            this.bictureIllustration.Location = new System.Drawing.Point(542, 90);
+            this.bictureIllustration.Name = "bictureIllustration";
+            this.bictureIllustration.Size = new System.Drawing.Size(446, 449);
+            this.bictureIllustration.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bictureIllustration.TabIndex = 1;
+            this.bictureIllustration.TabStop = false;
+            this.bictureIllustration.Click += new System.EventHandler(this.bictureIllustration_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Library_Management_System.Properties.Resources.google_icon_icons_com_62736;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(109, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(215, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.bictureIllustration);
+            this.Controls.Add(this.panelLogin);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.flowLayoutPanelIcons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bictureIllustration)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox bictureIllustration;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelIcons;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.LinkLabel linkForgot;
+        private System.Windows.Forms.CheckBox chkRemember;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ Library Management System/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+
+namespace Library_Management_System
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.Paint += MainForm_Paint;
+            panelLogin.Paint += PanelLogin_Paint;
+
+        }
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rect = this.ClientRectangle;
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                rect,
+                Color.FromArgb(255, 240, 250, 255), // لون علوي فاتح
+                Color.FromArgb(255, 250, 255, 255), // لون سفلي أبيض مقارب
+                0f)) // زاوية التدرج (0 = أيسر ليمين)
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }
+
+            // يمكنك إضافة تأثيرات ناعمة إضافية (super light)
+            using (LinearGradientBrush topGlow = new LinearGradientBrush(
+                new Rectangle(0, 0, rect.Width, 120),
+                Color.FromArgb(120, 200, 245, 255),
+                Color.FromArgb(0, Color.White),
+                90f))
+            {
+                e.Graphics.FillRectangle(topGlow, new Rectangle(0, 0, rect.Width, 120));
+            }
+        }
+
+        // رسم الظل وزوايا منحنية للـ panelLogin
+        private void PanelLogin_Paint(object sender, PaintEventArgs e)
+        {
+            Control ctrl = sender as Control;
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            int cornerRadius = 16;
+            Rectangle rect = ctrl.ClientRectangle;
+
+            // رسم ظل خفيف (بمقدار إزاحة صغيرة)
+            Rectangle shadowRect = new Rectangle(rect.X + 6, rect.Y + 6, rect.Width - 1, rect.Height - 1);
+            using (GraphicsPath gpShadow = RoundedRect(shadowRect, cornerRadius))
+            using (SolidBrush sb = new SolidBrush(Color.FromArgb(35, 0, 0, 0)))
+            {
+                e.Graphics.FillPath(sb, gpShadow);
+            }
+
+            // رسم خلفية الـ panel نفسها بزاوية منحنية
+            using (GraphicsPath gp = RoundedRect(rect, cornerRadius))
+            using (SolidBrush b = new SolidBrush(Color.White))
+            {
+                e.Graphics.FillPath(b, gp);
+            }
+        }
+
+        // دالة مساعدة لعمل مسار بمستطيل بزوايا منحنية
+        private GraphicsPath RoundedRect(Rectangle bounds, int radius)
+        {
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(bounds.Location, size);
+            GraphicsPath path = new GraphicsPath();
+
+            // أعلى يسار
+            path.AddArc(arc, 180, 90);
+
+            // أعلى يمين
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // أسفل يمين
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // أسفل يسار
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TextBox: إزالة الحدود الافتراضية وجعلها تبدو كحقل عصري
+            txtEmail.BorderStyle = BorderStyle.None;
+            txtPassword.BorderStyle = BorderStyle.None;
+
+            // إضافة خطوط تحت الحقول (line) باستخدام Panel صغير
+            Panel lineEmail = new Panel
+            {
+                Height = 1,
+                Width = txtEmail.Width,
+                BackColor = Color.LightGray,
+                Location = new Point(txtEmail.Left, txtEmail.Bottom + 6)
+            };
+            panelLogin.Controls.Add(lineEmail);
+
+            Panel linePass = new Panel
+            {
+                Height = 1,
+                Width = txtPassword.Width,
+                BackColor = Color.LightGray,
+                Location = new Point(txtPassword.Left, txtPassword.Bottom + 6)
+            };
+            panelLogin.Controls.Add(linePass);
+
+            // زر تسجيل دخول
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.BackColor = Color.FromArgb(22, 35, 64); // نفسجي/كحلي داكن
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Height = 40;
+            btnLogin.Width = 300;
+            btnLogin.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            btnLogin.Cursor = Cursors.Hand;
+        }
+
+        private void panelLogin_Paint(object sender, PaintEventArgs e)
+        {
+                
+        }
+
+        private void bictureIllustration_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
